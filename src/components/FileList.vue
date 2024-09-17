8119f272005e31a7fc3a6f7c3c9583ce88687ab5
Sware <shangfire@gmail.com>
2024-09-18 01:26:05 +0800

    update

---
+++ src/components/FileList.vue
@@ -0,0 +1,24 @@
+<template>  
+  <ul>  
+    <li v-for="file in files" :key="file.path">  
+      {{ file.name }}  
+      <button v-if="file.type === 'folder'" @click="enterFolder(file.path)">进入</button>  
+      <button v-else @click="deleteFile(file.path)">删除</button>  
+    </li>  
+  </ul>  
+</template>  
+  
+<script>  
+export default {  
+  name: 'FileList',  
+  props: ['files', 'currentPath'],  
+  methods: {  
+    enterFolder(path) {  
+      this.$emit('enter-folder', path);  
+    },  
+    deleteFile(filePath) {  
+      this.$emit('delete-file', filePath);  
+    }  
+  }  
+};  
+</script>

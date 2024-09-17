7395f8d2d250c601595e8afc8c3c896dc59a0e63
Sware <shangfire@gmail.com>
2024-09-18 01:21:21 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,52 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+<template>  
+  <div id="app">  
+    <nav>  
+      <button @click="goToParent">返回</button>  
+      <input type="text" disabled :value="currentPath" />  
+      <button @click="newFolder">新建文件夹</button>  
+      <button @click="uploadFile">上传文件</button>  
+    </nav>  
+    <file-list :files="files" :current-path="currentPath" @delete-file="handleDeleteFile" @enter-folder="handleEnterFolder"></file-list>  
+  </div>  
+</template>  
+  
+<script>  
+import FileList from './components/FileList.vue';  
+  
+export default {  
+  name: 'App',  
+  components: {  
+    FileList  
+  },  
+  data() {  
+    return {  
+      currentPath: '/',  
+      files: [  
+        // 示例数据  
+        { name: 'Folder1', type: 'folder', path: '/Folder1' },  
+        { name: 'File1.txt', type: 'file', path: '/File1.txt' },  
+        // 更多文件...  
+      ]  
+    };  
+  },  
+  methods: {  
+    goToParent() {  
+      // 实现返回上一级目录的逻辑  
+    },  
+    newFolder() {  
+      // 实现新建文件夹的逻辑  
+    },  
+    uploadFile() {  
+      // 实现上传文件的逻辑  
+    },  
+    handleDeleteFile() {  
+      // 处理删除文件的逻辑  
+    },  
+    handleEnterFolder(newPath) {  
+      // 处理进入文件夹的逻辑  
+      this.currentPath = newPath;  
+      // 更新文件列表  
+    }  
+  }  
+};  
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
